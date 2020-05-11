ba951713715430a030818674a9f7c3376f4cd01d
oldman3483 <qazws3483@gmail.com>
2020-05-11 17:34:22 +0800

    VR survey sample1

---
+++ VR_survey_proj/Assets/totalStation.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class totalStation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float m_HorizontalAngle = 0;
+    float m_VerticalAngle = 0;
+    const float ROTATE_SPEED = 40;
+
+
+    [SerializeField] GameObject m_base;
+    [SerializeField] GameObject m_telescope;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            m_HorizontalAngle += ROTATE_SPEED * Time.deltaTime;
+
+
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            m_HorizontalAngle += -ROTATE_SPEED * Time.deltaTime;
+        }
+
+
+
+
+        m_base.transform.localEulerAngles = new Vector3(0, 0, m_HorizontalAngle);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+
+            m_VerticalAngle += ROTATE_SPEED * Time.deltaTime;
+
+
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            m_VerticalAngle += -ROTATE_SPEED * Time.deltaTime;
+        }
+
+
+
+
+        m_telescope.transform.localEulerAngles = new Vector3(m_VerticalAngle, 0, 0);
+    }
+}
---
+++ VR_survey_proj/Assets/tripod.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tripod : MonoBehaviour
+{
+
+    [SerializeField] GameObject m_pivot01;
+    [SerializeField] GameObject m_pivot02;
+    [SerializeField] GameObject m_pivot03;
+
+    float m_OpenAngle01 = 0;
+    float m_OpenAngle02 = 0;
+    float m_OpenAngle03 = 0;
+
+    const float OPEN_SPEED = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.O))
+        {
+            m_OpenAngle01 += OPEN_SPEED * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.P))
+        {
+            m_OpenAngle01 += -OPEN_SPEED * Time.deltaTime;
+        }
+        m_pivot01.transform.localEulerAngles = new Vector3(m_OpenAngle01, 0, 0);
+
+        if (Input.GetKey(KeyCode.U))
+        {
+            m_OpenAngle02 += OPEN_SPEED * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.I))
+        {
+            m_OpenAngle02 += -OPEN_SPEED * Time.deltaTime;
+        }
+        m_pivot02.transform.localEulerAngles = new Vector3(m_OpenAngle02, m_OpenAngle02, 240);
+
+        if (Input.GetKey(KeyCode.Y))
+        {
+            m_OpenAngle03 += OPEN_SPEED * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.T))
+        {
+            m_OpenAngle03 += -OPEN_SPEED * Time.deltaTime;
+        }
+        m_pivot03.transform.localEulerAngles = new Vector3(0, m_OpenAngle03, 120);
+    }
+}
